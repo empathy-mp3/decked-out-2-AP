1fb346cc8caccfdb2e89051286c876ed22f16097
empathy-mp3 <mnemnth@icloud.com>
2024-07-09 14:22:32 -0700

    update yaml options with hooks/Options.py

--- manual_deckedout2_mp3/hooks/Options.py
+++ manual_deckedout2_mp3/hooks/Options.py
@@ -26,18 +26,28 @@ from ..Helpers import is_option_enabled, get_option_value
 # To add an option, use the before_options_defined hook below and something like this:
 #   options["total_characters_to_win_with"] = TotalCharactersToWinWith
 #
-class TotalCharactersToWinWith(Range):
-    """Instead of having to beat the game with all characters, you can limit locations to a subset of character victory locations."""
-    display_name = "Number of characters to beat the game with before victory"
-    range_start = 10
-    range_end = 50
-    default = 50
+class FillerTrapPercent(Range):
+    """What percentage of Moments of Clarity (filler) should be replaced by Stumbles (traps)?
+    0 means no Stumbles, 100 means all Moments of Clarity are replaced with Stumbles."""
+    range_end = 100
 
+class Legendary(Toggle):
+    """Should there be locations for the Legendary Cards (which are much harder to get, but a lot more interesting, logic-wise!)
+    Don't disable both this and Sixth_Locations."""
+    display_name = "Legenary Card Locations"
+
+class Sixth(Toggle):
+    """Should there be 6 locations for each purchase, or 5?
+    Don't disable both this and Legendary_Card_Locations."""
+    display_name = "Action Rando"
 
 # This is called before any manual options are defined, in case you want to define your own with a clean slate or let Manual define over them
 def before_options_defined(options: dict) -> dict:
+    options["Legendary_Card_Locations"] = Legendary
+    options["Sixth_Locations"] = Sixth
     return options
 
 # This is called after any manual options are defined, in case you want to see what options are defined or want to modify the defined options
 def after_options_defined(options: dict) -> dict:
+    options["filler_traps"] = FillerTrapPercent
     return options
